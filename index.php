20bfdb948659f5a3635f4f27bb7404520f073b0f
Shubhamarya755 <112922970+Shubhamarya755@users.noreply.github.com>
2025-04-03 16:24:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,209 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title> eton</title>
+	<style>
+		.container{
+			width: 90%;
+			margin: auto;
+		}
+		.col-6{
+			width: 50%;
+			float: left;
+		}
+		.col-4{
+			width: 33.33%;
+			float: left;
+		}
+		.col-3{
+			width: 25%;
+			float: left;
+		}
+		.col-2{
+			width: 16.66%;
+			float: left;
+		}
+		.col{
+			width: 100%;
+		}
+		.row{
+			overflow: hidden;
+			
+		}
+		.heading{
+			color: white;
+
+		}
+
+		.language{
+			text-align: center;
+		width: 30%;
+		background-color: white;
+		border-radius:50px;
+		margin: auto;
+		}
+		.menu-box{
+			
+		}
+		.booking{
+			border: 1px solid black;
+			width: 100%;
+
+			background-color: yellow;
+		}
+
+		.box{
+			display: inline-block;
+			margin: 1px;
+			padding: 1px;
+		}
+		.banner{
+			width: 100%;
+			text-align: center;
+			
+		}
+		.banner img{
+			width: 100%;
+		}
+		.container-box{
+			width: 80%;
+			height: 450px;
+			border: 1px solid black;
+			text-align: center;
+			margin: 10px;
+			padding: 5px;
+			box-shadow: 2px 2px 2px 2px grey;
+		}
+		.container-box img{
+			margin: 40px auto;
+		}
+		.container-box p{
+			margin: 40px auto;
+		}
+		.container-box .btn{
+			text-decoration: none;
+			border: 1px solid grey;
+			border-radius: 50px;
+			margin: 30px auto;
+			width: 40%;
+			height: 30px;
+			background-color: whitesmoke;
+		}
+
+
+	</style>
+</head>
+<body>
+	<!-- 100 -->
+	<div class="top">
+		<div class="container">
+			<div class="row" style="background-color: navy; ">
+				<div class="col-6">
+					<div class="language" >
+						<p>SELECT LANGUAGE</p>
+					</div>
+				</div>
+				<div class="col-6">
+					<div class="heading">
+						<p>  pepmindggn@gmail.com     7303071130    |    Contact Us    |    Franchise</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- 200 -->
+	<div class="container">
+		<!-- 210 -->
+		<div class="row" >
+			<!-- 211 -->
+			<div class="col-3">
+				<!--  211 logo -->
+				<div class="logo">
+					<img src="logo.png">
+				</div>
+			</div>
+			<!--212-->
+			<div class="col-6">
+				<!--212 menu-box  -->
+				<div class="menu-box">
+					<div class="box">
+						<div class="menu">
+							<p>Programs</p>
+						</div>
+					</div>
+
+					<div class="box">
+						<div class="menu">
+							<p>Company</p>
+						</div>
+					</div>
+
+					<div class="box">
+						<div class="menu">
+							<p>Testimonials</p>
+						</div>
+					</div>
+
+					<div class="box">
+						<div class="menu">
+							<p>Gallery</p>
+						</div>
+					</div>
+
+				</div>
+			</div>
+			<div class="col-2">
+				<div class="booking">
+					<p>Book A Free Trial </p>
+				</div>
+			</div>
+		</div>
+	</div>
+<img src="banner.png" width="100%">
+
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div class="" style="text-align: center;">
+					<div>
+						<h2>Our <span style="color:orangered;">Programs</span></h2>.
+					</div>
+					<div>
+						<p>Every child is born to be a leader. Some might have more visible potential correlated with leadership, but we believe and child's</p> 
+						<p>leadership ability, at whatever level, can be trained and nurtured.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="container">
+		<div class="row">
+			<div class="col-4">
+				<div class="container-box" >
+					<img src="pic-03.png">
+					<p>have unmatchable energy and transform yourself into a future leader.our Be more productive, upscale your potential </p>
+					<button class="btn"> READ HERE. >>></button>
+				</div>
+			</div>
+			<div class="col-4">
+				<div class="container-box" >
+					<img src="pic-03.png">
+					<p>have unmatchable energy and transform yourself into a future leader.our Be more productive, upscale your potential </p>
+					<button class="btn"> READ HERE. >>></button>
+				</div>
+			</div>
+			<div class="col-4">
+				<div class="container-box" >
+					<img src="pic-03.png">
+					<p>have unmatchable energy and transform yourself into a future leader.our Be more productive, upscale your potential </p>
+					<button class="btn"> READ HERE. >>></button>
+				</div>
+			</div>
+		</div>
+
+	</div>
+</body>
+</html>
